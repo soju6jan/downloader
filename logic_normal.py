5d0dfe55f2792381fdbcec74ef9a7c83112cabfa
soju6jan <cybersol@naver.com>
2020-08-15 18:07:49 +0900

    1

--- logic_normal.py
+++ logic_normal.py
@@ -259,13 +259,15 @@ class LogicNormal(object):
             logger.debug(data)
             from bot_downloader_ktv.model import ModelBotDownloaderKtvItem
             flag = ModelBotDownloaderKtvItem.receive_share_data(data)
-
+            logger.debug('process_telegram_data ktv : %s', flag)
             if not flag:
                 from bot_downloader_movie.model import ModelMovieItem
                 flag = ModelMovieItem.receive_share_data(data)
+                logger.debug('process_telegram_data movie : %s', flag)
             if not flag:
                 from bot_downloader_av.model import ModelItem
                 flag = ModelItem.receive_share_data(data)
+                logger.debug('process_telegram_data av : %s', flag)
         except Exception as e: 
             logger.error('Exception:%s', e)
             logger.error(traceback.format_exc()) 
