c50518663aea13a24c08fa3188de6a99a2b9dd0a
soju6jan <cybersol@naver.com>
2021-04-20 00:38:32 +0900

    1

--- logic_normal.py
+++ logic_normal.py
@@ -250,17 +250,26 @@ class LogicNormal(object):
     def process_telegram_data(data):
         try:
             if data['content_type'] == 'ktv':
-                from bot_downloader_ktv.model import ModelBotDownloaderKtvItem
-                flag = ModelBotDownloaderKtvItem.receive_share_data(data)
-                logger.debug('process_telegram_data ktv : %s', flag)
+                try:
+                    from bot_downloader_ktv.model import ModelBotDownloaderKtvItem
+                    flag = ModelBotDownloaderKtvItem.receive_share_data(data)
+                    logger.debug('process_telegram_data ktv : %s', flag)
+                except:
+                    pass
             elif data['content_type'] == 'movie':
-                from bot_downloader_movie.model import ModelMovieItem
-                flag = ModelMovieItem.receive_share_data(data)
-                logger.debug('process_telegram_data movie : %s', flag)
+                try:
+                    from bot_downloader_movie.model import ModelMovieItem
+                    flag = ModelMovieItem.receive_share_data(data)
+                    logger.debug('process_telegram_data movie : %s', flag)
+                except:
+                    pass
             elif data['content_type'] == 'av':
-                from bot_downloader_av.model import ModelItem
-                flag = ModelItem.receive_share_data(data)
-                logger.debug('process_telegram_data av : %s', flag)
+                try:
+                    from bot_downloader_av.model import ModelItem
+                    flag = ModelItem.receive_share_data(data)
+                    logger.debug('process_telegram_data av : %s', flag)
+                except:
+                    pass
         except Exception as e: 
             logger.error('Exception:%s', e)
             #logger.error(traceback.format_exc())
